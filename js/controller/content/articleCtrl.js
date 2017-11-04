33850f9e2c288ae9f2fc841d4295874629fa0c29
GodYeung <2506597416@qq.com>
2017-11-04 16:00:03 +0800

    内容分离

--- js/controller/content/articleCtrl.js
+++ js/controller/content/articleCtrl.js
@@ -1,28 +1,19 @@
-blog.controller("articleCtrl",['$scope','$http',function ($scope,$http) {
-    $scope.helloText={
-        title:'Hello world!',
-        content:'GIT不仅仅是个版本控制系统，它也是个内容管理系统(CMS),工作管理系统等。\n' +
-        '\n' +
-        '如果你是一个具有使用SVN背景的人，你需要做一定的思想转换，来适应GIT提供的一些概念和特征。\n' +
-        '\n' +
-        'Git 与 SVN 区别点：',
-        code:'                /**\n' +
-        '                * @author y15079\n' +
-        '                * @create 2017-11-03 16:36\n' +
-        '                * @desc\n' +
-        '                *\n' +
-        '                * LineNumberReader比BufferedReader多了个功能，就是可以返回当前行号。\n' +
-        '                * 另外setLineNumber能改变行号，却不能改变读的位置。\n' +
-        '                *\n' +
-        '                **/\n' +
-        '                public class StringDemo {\n' +
-        '                    public static void main(String args[]) {\n' +
-        '                       String site = "www.runoob.com";\n' +
-        '                       int len = site.length();\n' +
-        '                       System.out.println( "菜鸟教程网址长度 : " + len );\n' +
-        '                    }\n' +
-        '                }'
-    };
+blog.controller("articleCtrl",['$scope','$http','$location',function ($scope,$http,$location) {
+    var absUrl=$location.absUrl();
+    var path=$location.path();
+    var url=$location.url();
+    var hash=$location.hash();
+    var host=$location.host();
+    var port=$location.port();
+    console.log("absUrl: "+absUrl);
+    console.log("path: "+path);
+    console.log("hash; "+hash);
+    console.log("url: " +url);
+    console.log(host+port);
+    $http.get(host+"/article/article.json").then(function (response) {
+        $scope.hello=response.data.helloworld;
+    });
+    // $scope.hello="article/2017-11-04/2017-11-04.html";
     $scope.editorOptions={
         lineNumbers: true,
         readOnly: 'nocursor',
